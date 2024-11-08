548c4b5dec3273926310e2601f63be7ef97bbd69
Amrit Rijal <rijalamrit720@gmail.com>
2024-11-08 09:44:44 +0545

    added new folder and files

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,11 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { SplashScreen, Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+
+SplashScreen.preventAutoHideAsync();
 
 const RootLayout = () => {
+  const [fontsLoaded, fontsError] = useFonts({
+    "Poppins-Black": require("../assets/fonts/Poppins-Black.ttf"),
+    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
+    "Poppins-ExtraLight": require("../assets/fonts/Poppins-ExtraLight.ttf"),
+    "Poppins-Light": require("../assets/fonts/Poppins-Light.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "Poppins-ExtraBold": require("../assets/fonts/Poppins-ExtraBold.ttf"),
+  })
+
+  useEffect(() => {
+    if(fontsError) throw fontsError;
+    if(fontsLoaded) SplashScreen.hideAsync(); 
+  }), [fontsLoaded, fontsError];
+
+  if(!fontsLoaded && !fontsError) return null;
+
   return (
     <Stack>
-        <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name='index' options={{ headerShown: false }} />
     </Stack>
   )
 }
