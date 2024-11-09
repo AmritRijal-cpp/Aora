a58c2e877080a9f934ebdcc3aa9c46dfd1bb640c
Amrit Rijal <rijalamrit720@gmail.com>
2024-11-09 22:09:59 +0545

    Added frontpage logo and card image

--- app/index.jsx
+++ app/index.jsx
@@ -1,15 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { Link } from 'expo-router';
 import "../global.css";
-
+import { SafeAreaView } from 'react-native-safe-area-context';
+import {images} from '../constants';
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl font-pblack">Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
-    </View>
+    <SafeAreaView classname="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <View classname="w-full justify-center items-center h-full px-4">
+          <Image 
+            source={images.logo}
+            classname="w-[130px] h-[84px]"
+            resizeMode="contain"  
+          />
+          <Image 
+            source={images.cards}
+            classname="max-w-[380px] w-full h-[300px]"
+            resizeMode="contain"
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
