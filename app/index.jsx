8ba56bee157d3a4fe4a29bc5249fa8e54c02a386
Amrit Rijal <rijalamrit720@gmail.com>
2024-11-11 22:58:56 +0545

    Completed Frontpage

--- app/index.jsx
+++ app/index.jsx
@@ -1,27 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import { Image, ScrollView, StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import { Link, router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import '../global.css';
-import {images} from '../constants';
+import { images } from '../constants';
+import CustomButton from '../components/CustomButton';
 export default function App() {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: '100%' }}>
-        <View className="w-full justify-center items-center h-full px-4">
-          <Image 
+        <View className="w-full justify-center items-center
+         min-h-[85vh] px-4">
+          <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
-            resizeMode="contain"  
+            resizeMode="contain"
           />
-          <Image 
+          <Image
             source={images.cards}
             className="max-w-[380px] w-full h-[300px]"
             resizeMode="contain"
           />
-          <Link href="/profile" className='text-white'>Profile</Link>
+          <View className="relative mt-5">
+            <Text className="text-[33px] text-white 
+            font-bold text-center">Discover Endless
+              Possibilities with {' '}
+              <Text className="text-secondary-200">
+                Aora
+              </Text>
+            </Text>
+            <Image
+              source={images.path}
+              className="w-[136px] h-[15px] absolute
+              -bottom-2 -right-9"
+              resizeMode='contain'
+            />
+          </View>
+          <Text className="text-sm font-pregular 
+          text-gray-100 mt-7 text-center">
+            Where creativity meets innovation: embark
+            on a journey of the limitless exploration with
+            Aora
+          </Text>
+          {/* <Link href="/profile" className='text-white'>
+            Profile
+          </Link> */}
+          <CustomButton 
+            title="Countinue with Email"
+            handlePress={()=> router.push('/sign-in')}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+
+      <StatusBar style='light' backgroundColor='#161622' />
     </SafeAreaView>
   );
 }
