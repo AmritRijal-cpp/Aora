9c0c55fe9f3f717d69b201d47f9be342b3eeb728
Amrit Rijal <rijalamrit720@gmail.com>
2024-11-21 16:00:09 +0545

    completed profile tab

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -9,11 +9,13 @@ import EmptyState from '../../components/EmptyState'
 import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard'
+import GlobalProvider, { useGlobalContext } from '../../context/GlobalProvider'
 
 const Home = () => {
   const [refreshing, setRefreshing] = useState(false);
   const { data: posts, refetch, isLoading } = useAppwrite(getAllPosts);
   const { data: latestPosts} = useAppwrite(getLatestPosts);
+  const {user} = useGlobalContext();
   
   const onRefresh = async () => {
     setRefreshing(true);
@@ -34,10 +36,10 @@ const Home = () => {
             <View className="justify-between items-start flex-row mb-6">
               <View>
                 <Text className="font-pmedium text-sm text-gray-100">
-                  Welcome Back,
+                  Welcome back,
                 </Text>
                 <Text className="text-2xl font-psemibold text-white">
-                  Amrit
+                  {user?.username}
                 </Text>
               </View>
               <Image
