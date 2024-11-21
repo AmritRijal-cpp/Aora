15d737310f9614961bb4c1fc0ee03b1fd8dcd44a
Amrit Rijal <rijalamrit720@gmail.com>
2024-11-21 22:01:37 +0545

    added functionality to create page

--- app/(tabs)/create.jsx
+++ app/(tabs)/create.jsx
@@ -1,33 +1,127 @@
-import { useVideoPlayer, VideoView } from 'expo-video';
-import { StyleSheet, View, Button } from 'react-native';
-
-const videoSource =
-  '"https://www.w3schools.com/html/mov_bbb.mp4"';
-export default function Create() {
-  const player = useVideoPlayer(videoSource, player => {
-    player.loop = true;
-    player.play();
+import { ScrollView, View, Text, TouchableOpacity, Image, Alert } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import FormField from '../../components/FormField';
+import { useState } from 'react';
+import { ResizeMode, Video } from 'expo-av';
+import * as DocumentPicker from 'expo-document-picker';
+import { icons } from '../../constants';
+import CustomButton from '../../components/CustomButton'
+const Create = () => {
+  const [uploading, setUploading] = useState(false);
+  const [form, setForm] = useState({
+    title: '',
+    video: null,
+    thumbnail: null,
+    prompt: ''
   });
+
+  const openPicker = async (selectType) => {
+    const result = await DocumentPicker.getDocumentAsync({
+      type: selectType === 'image' ?
+        ['image/*'] : ['video/*']
+    })
+
+    if(!result.canceled) {
+      if(selectType==="image") {
+        setForm({...form, thumbnail: result.assets[0]});
+      } if(selectType==="video"){
+        setForm({...form, video: result.assets[0]});
+        console.log(form.video);
+        
+      }
+    } else {
+      setTimeout(()=>{
+        Alert.alert('Document Picked', JSON.stringify(result, null, 2))
+      }, 100)
+    }
+  }
+  const submit = () => {
+
+  }
   return (
-    <View style={styles.contentContainer}>
-      <VideoView style={styles.video} player={player} allowsFullscreen allowsPictureInPicture />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView className="px-4 my-6">
+        <Text className="text-2xl text-white font-psemibold">
+          Upload Video
+        </Text>
+
+        <FormField
+          title="Video title"
+          value={form.title}
+          placeholder="Give your video a title"
+          handleTextChange={(e) => setForm({ ...form, title: e })}
+          otherStyles="mt-10"
+        />
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-green-100 font-pmedium">
+            Upload Video
+          </Text>
+          <TouchableOpacity
+            onPress={() => openPicker('video')}
+          >
+            {form.video ? (
+              <Video
+                source={{ uri: form.video.uri }}
+                className="w-full h-64 rounded-2xl"
+                useNativeControls
+                resizeMode={ResizeMode.COVER}
+                isLooping
+              />
+            ) : (
+              <View className="w-full h-40 px-4 bg-black-100 rounded-2xl justify-center items-center">
+                <View className="w-14 h-14 border border-dashed border-secondary-100 justify-center items-center">
+                  <Image
+                    source={icons.upload}
+                    resizeMode='contain'
+                    className="w-1/2 h-1/2"
+                  />
+                </View>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+        <View className="mt-7 space-y-2">
+          <Text className="text-base text-green-100 font-pmedium">
+            Thumbnail Image
+          </Text>
+          <TouchableOpacity
+            onPress={() => openPicker('image')}
+          >
+            {form.thumbnail ? (
+              <Image
+                source={{ uri: form.thumbnail.uri }}
+                resizeMode='cover'
+                className="w-full h-64 rounded-2xl"
+              />
+            ) : (
+              <View className="w-full h-16 px-4 bg-black-100 rounded-2xl justify-center items-center border-2 border-black-100 flex-row space-x-2">
+                <Image
+                  source={icons.upload}
+                  resizeMode='contain'
+                  className="w-5 h-5"
+                />
+                <Text className="text-gray-100 font-pmedium ml-2">Choose a file...</Text>
+              </View>
+            )}
+          </TouchableOpacity>
+        </View>
+
+        <FormField
+          title="Prompt"
+          value={form.prompt}
+          placeholder="Prompt used to create the video"
+          handleTextChange={(e) => setForm({ ...form, prompt: e })}
+          otherStyles="mt-7"
+        />
+        <CustomButton
+          title="Submit"
+          handlePress={submit}
+          containerStyles="mt-7"
+          isLoading={uploading}
+        />
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  contentContainer: {
-    flex: 1,
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingHorizontal: 50,
-  },
-  video: {
-    width: 350,
-    height: 275,
-  },
-  controlsContainer: {
-    padding: 10,
-  },
-});
+export default Create
